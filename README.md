08a51b147c69d4550eb3e8d587e81b6c4b3eb1af
Olivia Conley <oliviarconley@gmail.com>
2022-07-26 13:04:17 -0400

    Modified JavaScript to properly achieve project goals

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Password-Generator-072522
+
+Technologies used: HTML, CSS, JavaScript, GitHub, VSCode
+## Summary 
+Modified starter code to create a password generator application, allowing the user to select the criteria they want to include in their password. 
+
+## Highlights
+* Implements flexbox to accomodate various screen types
+* 
+
+## Goals 
+
+* Create a portfolio that I can develop over time, showcasing my work to future employers. 
+
+## Learning Experience
+
+* Practiced using advanced styling techniques to achieve desired portfolio look and feel. 
+
+## Links
+
+GitHub: https://github.com/oliviaconley
+
+LinkedIn: https://www.linkedin.com/in/olivia-conley/
--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-// Assignment Code
-// This is selecting the FIRST element that has an ID of "generate"
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-// This is selecting the FIRST element that has the ID of "password"
   var passwordText = document.querySelector("#password");
-// This is setting the text of the password variable 
-  passwordText.value = "password";
+  passwordText.value = password;
 
 }
 
@@ -22,33 +18,39 @@ var special = "!@#$%^&*<>?~`_-=+";
 var lettersLowercase = "abcdefghijklmnopqrstuvwxyz";
 var lettersUppercase = "ABCDEFGHIJKLMNOPQRZTUVWXYZ"; 
 
-//prompts 
-
-// Add prompt window for password length and which characters to include. 
-function writePassword() {
+function generatePassword() {
   var passwordLength = prompt("How many characters do you want your password to have?");
-  alert(passwordLength);
+  var storePassword = "";
+alert(passwordLength);
+
+if (passwordLength > 8 && passwordLength < 180) {
   var addNumber = confirm("Do you want to add numbers?");
   var addSpecial = confirm("Do you want to add special characters?");
-  var addLetter = confirm("Do you want to add numbers?");
-}
-
-// This is writing out the permutation of characters based on user's selection. 
-// So this says if they confirm add number, special or letter.... 
-
-for (let i = 0; i <= passwordLength; i++) {
-  
+  var addUpperCase = confirm("Do you want to add uppercase letters?");
+  var addLowerCase = confirm("Do you want to add lowercase letters?");
+  var realPassword = ""; 
   if (addNumber) {
-
+    storePassword += digits;
   } if (addSpecial) {
-
-  } if (addLetter) {
-
+    storePassword += special;  
+  } if (addUpperCase) {
+    storePassword += lettersUppercase;
+  } if (addLowerCase) {
+    storePassword += lettersLowercase;
   }
+
+  for (let i = 0; i < passwordLength; i++) {
+    realPassword += storePassword [Math.floor(Math.random() * storePassword.length)]
+  }
+
+} else {
+  alert("Try again please!")
+  generatePassword ();
 }
 
-function generateFinalPw () {
-  console.log()
+console.log(realPassword.length)
+return realPassword;
 }
+
 
 
